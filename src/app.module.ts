5a9e46a857c284da2b7b2cde53e05cf9ddedc727
chamarez <michel.chamarez@gmail.com>
2021-11-18 00:48:15 +0100

    Server working with docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+      host: 'localhost',
+      port: 5000,
+      username: 'root',
+      password: 'root',
+      database: 'database',
+      autoLoadEntities: true,
+      synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
