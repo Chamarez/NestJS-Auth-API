13ca99265c8f023fb005eb4a281c89ffbd5b3e72
chamarez <michel.chamarez@gmail.com>
2021-11-18 02:19:30 +0100

    Validations in dto

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,8 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from "class-validator";
+
 export class RegisterUserDto{
+    @IsNotEmpty()
+    @IsString()
+    @Length(2, 20)
     name:string;
 
+    @IsNotEmpty()
+    @IsEmail()
     email:string;
 
+    @IsNotEmpty()
+    @Length(6, 20)
     password:string;
 
 }
