123c70c560f25f6d83afa3693b0ceb78769d2377
chamarez <michel.chamarez@gmail.com>
2021-11-18 02:11:55 +0100

    Validation email already registered

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -9,7 +9,7 @@ export class User{
     @Column({length:20})
     name:string;
 
-    @Column({length:100})
+    @Column({length:100, unique:true})
     email:string;
     
     @Column({length:100})
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { RegisterUserDto } from "./dto/register-user.dto";
 import { User } from "./user.entity";
@@ -8,7 +9,21 @@ export class UserRepository extends Repository<User> {
    async createUser(registerUserDto:RegisterUserDto): Promise<void> {
        const {name, email, password} = registerUserDto;
        const user = this.create({name, email, password});
-       await this.save(user)
+       
+       try{
+
+        await this.save(user)
+       } catch(e){
+        if(e.code === 'ER_DUP_ENTRY'){
+            throw new ConflictException('This email is already registered')
+        }
+        throw new InternalServerErrorException();
+       }
+       
+
+
+
+
    }
 
 }
